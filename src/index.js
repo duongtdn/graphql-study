06ed9b23cea9259dd60eb1f4a5cfdccd76afd153
duongtdn <duongtdn@gmail.com>
2017-10-19 13:19:58 +0700

    setup with Apollo GraphQL

--- src/index.js
+++ src/index.js
@@ -3,13 +3,27 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 
+import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo'
+
+const networkInterface = createNetworkInterface({
+  uri: 'https://api.graph.cool/simple/v1/cj8xug9fk0aqt0128k0t51n78'
+});
+
+const client = new ApolloClient({
+  networkInterface
+});
+
+
+
 class App extends Component {
   constructor(props) {
     super(props);
   }
   render() {
     return (
-      <div> GraphQL </div>
+      <ApolloProvider client = {client} >
+        <div> GraphQL </div>
+      </ApolloProvider>
     );
   }
 }
