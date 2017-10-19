def40a1861976718d322d3e1f511ea40d4ae9583
duongtdn <duongtdn@gmail.com>
2017-10-19 13:54:30 +0700

    using query

--- src/LinkList.js
+++ src/LinkList.js
@@ -1,21 +1,34 @@
 "use strict"
 
 import React, { Component } from 'react'
+import { graphql, gql } from 'react-apollo'
+
 import Link from './Link'
+
+const ALL_LINKS_QUERY = gql`
+  query AllLinksQuery {
+    allLinks {
+      id
+      createdAt
+      url
+      description
+    }
+  }
+`
 
 class LinkList extends Component {
 
   render() {
 
-    const linksToRender = [{
-      id: '1',
-      description: 'The Coolest GraphQL Backend 😎',
-      url: 'https://www.graph.cool'
-    }, {
-      id: '2',
-      description: 'The Best GraphQL Client',
-      url: 'http://dev.apollodata.com/'
-    }]
+    if (this.props.allLinksQuery && this.props.allLinksQuery.loading) {
+      return <div>Loading...</div>
+    }
+
+    if (this.props.allLinksQuery && this.props.allLinksQuery.error) {
+      return <div>Error</div>
+    }
+
+    const linksToRender = this.props.allLinksQuery.allLinks;
 
     return (
       <div>
@@ -28,4 +41,4 @@ class LinkList extends Component {
 
 }
 
-export default LinkList
+export default graphql(ALL_LINKS_QUERY, { name: 'allLinksQuery' }) (LinkList)
