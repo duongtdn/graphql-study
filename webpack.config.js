839f31a20c5badf4f580ddead435bca2a3193819
duongtdn <duongtdn@gmail.com>
2017-10-19 09:15:29 +0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  
+  entry: {
+		/* built library */
+		lib: './dist/exporter.js',
+		/* demo pages */
+		page: './demo/lib/page.js',
+		navigator: './demo/lib/navigator.js',
+		sidebar: './demo/lib/sidebar.js',
+		tabs: './demo/lib/tabs.js',
+		toucharea: './demo/lib/toucharea.js',
+		form: './demo/lib/form.js',
+	}, 
+
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'demo', 'web', 'build')
+  },
+
+  module: {
+		rules : [{
+			test: /\.css$/, 
+			use: ExtractTextPlugin.extract({
+				use : 'css-loader'
+			})
+		}]
+	},
+	
+	plugins: [
+		new ExtractTextPlugin('bundle.css')
+	]
+
+}
